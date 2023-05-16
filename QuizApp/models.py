69a23269bf81874a35345ddccfb09aedb47b0beb
szymon191 <szymon366@vp.pl>
2023-05-16 09:47:26 +0200

    initial commit

---
+++ QuizApp/models.py
@@ -0,0 +1,34 @@
+import datetime
+
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    password = models.CharField(max_length=500)
+    isActive = models.BooleanField(default=True)
+    registrationDate = models.DateTimeField(auto_now_add=True)
+
+
+class Question(models.Model):
+    text = models.CharField(max_length=300)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    shuffleAnswers = models.BooleanField(default=True)
+
+class Answer(models.Model):
+    text = models.CharField(max_length=300)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class QuestionAnswers(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    asnwerOrder = models.IntegerField(default=1)
+    isCorrect = models.BooleanField()
+
+class Quiz(models.Model):
+    questions = models.ManyToManyField(Question)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+

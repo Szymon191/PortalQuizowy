46c098e7ef9fa938384a8bf11acd399d53235924
szymon191 <szymon366@vp.pl>
2023-05-16 10:16:45 +0200

    initial commit

--- QuizApp/models.py
+++ QuizApp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, blank=False, null=False)
     email = models.EmailField(max_length=100)
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
@@ -15,20 +15,23 @@ class User(models.Model):
 
 class Question(models.Model):
     text = models.CharField(max_length=300)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     shuffleAnswers = models.BooleanField(default=True)
 
 class Answer(models.Model):
     text = models.CharField(max_length=300)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
-class QuestionAnswers(models.Model):
+class QuestionAnswer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
     asnwerOrder = models.IntegerField(default=1)
-    isCorrect = models.BooleanField()
+    isCorrect = models.BooleanField(default=False)
 
 class Quiz(models.Model):
     questions = models.ManyToManyField(Question)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name_plural = 'Quizes'
 
